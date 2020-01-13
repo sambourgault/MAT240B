a1332d1120ffc2c6960204ae8833ae9dd4976d14
sam bourgault <samuelle.bourgault@gmail.com>
2020-01-12 22:02:56 -0800

    remove crap

--- build/my-first-app/Release/CMakeFiles/my-first-app_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/my-first-app_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/my-first-app_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
